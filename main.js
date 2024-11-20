a43796e472c727ccd50c657c295928ed2051b397
Douglas Mendes <douglasleomendes@hotmail.com>
2024-11-20 15:02:30 +0000

    add some style

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   mainWindow.loadFile('index.html');
+
+  // Criando o menu
+  const template = [
+    {
+      label: 'Game Store',
+      submenu: [
+        { label: 'Settings', click: () => console.log('Settings clicked') },
+        { type: 'separator' },
+        { label: 'Exit', role: 'quit' },
+      ],
+    },
+    {
+      label: 'Library',
+      submenu: [
+        { label: 'Games' },
+        { label: 'Software' },
+        { label: 'Music' },
+        { label: 'Tools' },
+      ],
+    },
+    {
+      label: 'Store',
+      submenu: [
+        { label: 'Featured' },
+        { label: 'Top Sellers' },
+        { label: 'New Releases' },
+      ],
+    },
+    {
+      label: 'Friends',
+      submenu: [
+        { label: 'View Friends List' },
+        { label: 'Chat' },
+      ],
+    },
+    {
+      label: 'Help',
+      submenu: [
+        { label: 'Support' },
+        { label: 'System Information' },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   mainWindow.on('closed', () => {
     mainWindow = null;
